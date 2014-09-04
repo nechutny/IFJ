0e21b2616584d83f54d4b3a93f6290b6cadf25d8
Petr Lukes <petrlukes7@gmail.com>
2014-09-04 22:41:05 +0200

    Pridavam zasobnik z lonska

---
+++ src/stack.c
@@ -0,0 +1,57 @@
+/********************************************************************/
+/*Projekt:Implementace interpretu imperativního jazyka IFJ13		*/
+/*Jména řešitelů: Michal Jásenský, Jiří Kulda ,Martin Filípek,      */
+/*                Ondřej Vlk, Pet Lukeš								*/
+/*Loginy řešitelů: xjasen00, xkulda00, xfilip32, xlukes06, xvlkon00 */
+/********************************************************************/
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include "types.h"
+#include "stack.h"
+#include "main.h"
+
+TStack * stack_init(){
+	TStack *stack;
+	stack = malloc( sizeof ( TStack ) );
+	if( stack == NULL ){
+		fprintf( stderr, "%s", strerror( errno ) );
+		return NULL;
+	}
+	stack->data = malloc( sizeof ( void * )*10 );
+	if( stack->data == NULL ){
+		fprintf( stderr, "%s", strerror( errno ) );
+		free( stack );
+		return NULL;
+	}
+
+	stack->allocated = 10;
+	stack->count = 0;
+	return stack;
+}
+
+void stack_push( TStack *stack, void *item ){
+	if( stack->allocated < ( stack->count + 1 ) ){
+		stack->data = realloc( stack->data, ( sizeof( void * ) * ( stack->allocated + 10 ) ) );
+		stack->allocated += 10;
+	}
+	stack->data[stack->count++] = item;
+}
+
+void stack_pop( TStack *stack ){
+	if( stack->count != 0 ){
+		stack->count--;
+	}	
+}
+
+void * stack_top( TStack *stack ){
+	
+	if( stack->count != 0 ){
+		return( stack->data[stack->count - 1] );
+	}else{
+		return NULL;
+	}
+
+}
---
+++ src/stack.h
@@ -0,0 +1,22 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include "types.h"
+
+typedef struct _stack {
+	void ** data;
+	int allocated;
+	int count;
+} TStack;
+
+TStack * stack_init();
+
+void stack_push( TStack *stack, void *item );
+
+void stack_pop( TStack *stack );
+
+void * stack_top( TStack *stack );
+
+void stack_free( TStack *stack );
+
+#endif
