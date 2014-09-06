c6e8567db00dd508f52311e6a8cee571e8d5995b
Jurisak <xkulda00@stud.fit.vutbr.cz>
2014-09-06 22:32:51 +0200

    added Jano data

--- src/main.c
+++ src/main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
+#include "lex.h"
+#include "types.h"
+
 int main(int argc, char* argv[])
 {
+	TToken * token;
+	FILE * file;
+	file=fopen("sourcetest.pas","r");
+	if (file==NULL)
+		return 0;
+	token = token_get(file);
+	while (token->type!=token_eof)
+	{
+		printf("%s ",token->data->data);
+		printf("%d\n",token->type);
+		token_free(token);
+		token = token_get(file);
+	}
+	fclose(file);
 	return 0;
 }
