da5705696853ef64420cfbc69d7fe6db06b8222d
Jurisak <xkulda00@stud.fit.vutbr.cz>
2014-09-07 10:40:16 +0200

    added lines to work with expr.c

--- src/main.c
+++ src/main.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-
 #include "lex.h"
 #include "types.h"
+#include "expr.h"
 
 int main(int argc, char* argv[])
 {
 	TToken * token;
 	FILE * file;
-	file=fopen("sourcetest.pas","r");
+	file=fopen(argv[1],"r");
 	if (file==NULL)
 		return 0;
+	precedence(file);
 	token = token_get(file);
 	while (token->type!=token_eof)
 	{
