ef615009568bb1fd7d900f967b1f346d1a698209
mcajsnk <mcajsnk@seznam.cz>
2014-09-07 20:26:31 +0200

    ocislovane typy tokenu

--- src/types.h
+++ src/types.h
@@ -15,48 +15,48 @@ typedef enum {
 	token_slash,		//6
 	token_mul,			//7
 	token_sub,			//8
-	token_div,
-	token_mod,
+	token_div,			//9
+	token_mod,			//10
 	
-	token_in,
-	token_dot,			//9
+	token_in,			//11
+	token_dot,			//12
 //comaring
-	token_equal,		//10
-	token_not_equal,	//11
-	token_less,			//12
-	token_less_equal,	//13
-	token_greater,		//14
-	token_greater_equal,//15
+	token_equal,		//13
+	token_not_equal,	//14
+	token_less,			//15
+	token_less_equal,	//16
+	token_greater,		//17
+	token_greater_equal,//18
 
-	token_int,			//16
-	token_double,		//17
-	token_eof,			//18
+	token_int,			//19
+	token_double,		//20
+	token_eof,			//21
 // ( )
-	token_parenthesis_left,	//19
-	token_parenthesis_right,//20
+	token_parenthesis_left,	//22
+	token_parenthesis_right,//23
 // ;
-	token_semicolon,		//21
+	token_semicolon,		//24
 //:
-	token_colon,			//22
+	token_colon,			//25
 
-	token_identifier,		//23
+	token_identifier,		//26
 // keywords
-	token_begin,			//24
-	token_end,				//25
-	token_while,
-	token_for,
-	token_if,
-	token_then,
-	token_else,
-	token_procedure,
-	token_function,
-	token_return,
-	token_true,
-	token_false,
-	token_null,
-	token_or,
-	token_and,
-	token_not,
+	token_begin,			//27
+	token_end,				//28
+	token_while,			//29
+	token_for,				//30
+	token_if,				//31
+	token_then,				//32
+	token_else,				//33
+	token_procedure,		//34
+	token_function,			//35
+	token_return,			//36
+	token_true,				//37
+	token_false,			//38
+	token_null,				//39
+	token_or,				//40
+	token_and,				//41
+	token_not,				//42
 // integrated functions not working yet (it depands on task from teachers)
 	token_f_boolval,
 	token_f_doubleval,
