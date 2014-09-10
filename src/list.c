fc8c9485125ae30acdd590af9c8d876b80091f71
Petr Lukes <petrlukes7@gmail.com>
2014-09-10 18:05:53 +0200

    funkcni seznam

--- src/list.c
+++ src/list.c
@@ -8,7 +8,7 @@
 TList* list_init()
 {
 	TList *list;
-	list = _malloc(sizeof(TList));
+	list = _malloc(sizeof(TList*));
 	list->first = NULL;
 	list->last = NULL;
 	list->act = NULL;
@@ -18,21 +18,33 @@ TList* list_init()
 void list_insert(TList *list, void *data)
 {
 	TNode *tmp;
-	_malloc(sizeof(TNode));
+	tmp = _malloc(sizeof(TNode));
 
 	tmp->data = data;
 	tmp->n = NULL;
-	tmp->p = list->act;
+	tmp->p = list->last;
+
+	if(list->last != NULL)	list->last->n = tmp;
+	if(list->first == NULL)	list->first = tmp;
+	if(list->act == NULL)	list->act = tmp;
+	
+	list->last = tmp;
 	return;
 }
 
 void list_insert_next(TList *list, void *data)
 {
 	TNode *tmp;
-	tmp = _malloc(sizeof(TNode))
+	tmp = _malloc(sizeof(TNode));
 
 	tmp->data = data;
 	tmp->p = list->act;
+	
+	if (list->act != NULL)
+	{	
+		tmp->n = list->act->n;
+		list->act->n = tmp;
+	}
 
 	if(list->first == NULL)
 	{
@@ -48,16 +60,19 @@ void list_insert_prev(TList *list, void *data)
 	if( list->act != NULL)
 	{
 		TNode *tmp;
-		tmp = _malloc(sizeof(TNode))
+		tmp = _malloc(sizeof(TNode));
 
 		tmp->data = data;
 		tmp->n = list->act;
-		tmp->p = list->act->p;
 
-		if( list->act->p != NULL)
-			list->act->p->n = tmp;
-		else
-			list->first = tmp;
+		if(list->act != NULL)
+		{
+			if( list->act->p != NULL)	list->act->p->n = tmp;
+			else						list->first = tmp;
+
+			tmp->p = list->act->p;
+			list->act->p = tmp;
+		}
 
 		list->act->p = tmp;
 	}
