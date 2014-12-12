89f8bf8394a7bdd5548ca75d9a7b0dd4cacd8ebf
mcajsnk <mcajsnk@seznam.cz>
2014-12-12 17:20:14 +0100

    par testiku na semantiku

---
+++ tests/error-semantic6.pas
@@ -0,0 +1,23 @@
+var cislo : integer;
+    cislo2 : integer;
+    desetine : real;
+    text : string;
+
+function funkce1( foo : integer) : integer; forward;
+
+function funkce2( arg1 : real; arg2 : integer ) : integer;
+begin
+    funkce2 := 123
+end;
+
+function funkce1( foo : integer) : integer;
+var xyz : real;
+begin
+    funkce1 := 1;
+    foo := 123
+end;
+
+begin
+    cislo := funkce1(3.2)
+
+end.
---
+++ tests/error-semantic8.pas
@@ -0,0 +1,23 @@
+var cislo : integer;
+    cislo2 : integer;
+    desetine : real;
+    text : string;
+
+function funkce1( foo : integer) : integer; forward;
+
+function sort( arg1 : real; arg2 : integer ) : integer;
+begin
+    funkce2 := 123
+end;
+
+function funkce1( foo : integer) : integer;
+var xyz : real;
+begin
+    funkce1 := 1;
+    foo := 123
+end;
+
+begin
+    cislo := funkce1(3)
+
+end.
